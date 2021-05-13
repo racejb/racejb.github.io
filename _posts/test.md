22cc2660b3f199d85b36f1b7e60c764ad32f3702
racejb <moonnr94@gmail.com>
2021-05-13 11:28:03 +0900

    config setting

--- _posts/test.md
+++
@@ -1,8 +0,0 @@
----
-title: "test!"
-data: 2021-05-13 11:05:00 -0400
-tags: [test,test2]
-toc: true
-toc_label: "My Table of Contents"
-toc_icon: "cog" # 내 컨텐츠에 대한 목차를 오른쪽에 띄워줌
----
---
+++ my-gatsby-project/gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `RACEJB's Blog`,
+  description: `Develop, BlockChain, It, Etc...`,
+  author: ``,
+  introduction: `신입 프론트 개발자입니다~`,
+  siteUrl: `https://racejb.github.io/`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `racejb`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: ``, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    utterances: "RAEJB/racejb.github.io", // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: "racejb",
+  },
+  share: {
+    facebookAppId: "", // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: "", // Add your google analytics tranking ID
+  // jennifer_front_id: "ed243e67", // Add your jennifer front tranking ID
+}
