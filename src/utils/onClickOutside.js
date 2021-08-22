01d0a519a698ca01270230c90144b676b6dc60e5
racejb <moonnr94@gmail.com>
2021-08-22 09:20:24 +0900

    re update

--- src/utils/onClickOutside.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import onClickOutside from "react-onclickoutside";
-
-class Clickoutside extends Component {
- 
-}
-
-export default onClickOutside(Clickoutside);
